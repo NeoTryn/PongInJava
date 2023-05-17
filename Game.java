52bd17936e8592c83af2ee8827997167dddb518f
Shadet <133360895+Shadet897@users.noreply.github.com>
2023-05-17 19:04:04 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 /*
 - Collision (Felix)
-- Movement (Kornel)
+- Movement (Kornel) // in Progress
 - Score
 - UI
 - Screens (Game over, Start, Menu)
@@ -16,6 +17,15 @@ public class Game {
 
     public static final int SCREEN_WIDTH = 1280;
     public static final int SCREEN_HEIGHT = 720;
+
+    public static int player1X = 20 ;
+    public static int player1Y = 260;
+    public static int player2X = 1215;
+    public static int player2Y = 260;
+
+    public static boolean isRunning = true;
+
+    public static final Set<Integer> pressedKeys = new HashSet<>();
 
     private Window window = new Window();
 
