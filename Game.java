fb1c487fc1d0671b68ff045e3ba54417fc0f24a8
NeoTryn <114293865+NeoTryn@users.noreply.github.com>
2023-05-17 13:17:32 +0200

    updated Game.java for pong

--- Game.java
+++ Game.java
@@ -1,3 +1,25 @@
-//fucku
-//Hello
-Hs
+import javax.swing.*;
+import java.awt.*;
+
+/*
+- Collision (Felix)
+- Movement (Kornel)
+- Score
+- UI
+- Screens (Game over, Start, Menu)
+- Themes
+- (Pick-Ups)
+- Physics
+ */
+
+public class Game {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+
+    private Window window = new Window();
+
+    public Game() {
+
+    }
+}
