9276f19661927d54c63aaadafc42957f831ab9b1
Shadet <133360895+Shadet897@users.noreply.github.com>
2023-05-17 19:02:44 +0200

    Update Movement.java

--- Movement.java
+++ Movement.java
@@ -2,36 +2,30 @@ import java.awt.event.*;
 
 public class Movement implements ActionListener {
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
 
     }
-    public static class MyKeyAdapter extends KeyAdapter{
+    public static class Player1KeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
-            switch (e.getKeyCode()){
-                case KeyEvent.VK_W -> {
-                    if (Game.player1Y > 0){
-                        Game.player1Y -= 15;
-                    }
-                }
-                case KeyEvent.VK_S -> {
-                    if (Game.player1Y + 170 < Game.SCREEN_HEIGHT){
-                        Game.player1Y += 15;
-                    }
-                }
-                case KeyEvent.VK_UP -> {
-                    if (Game.player2Y > 0){
-                        Game.player2Y -= 15;
-                    }
-                }
-                case KeyEvent.VK_DOWN -> {
-                    if (Game.player2Y + 170 < Game.SCREEN_HEIGHT){
-                        Game.player2Y += 15;
-                    }
-                }
-            }
+            Game.pressedKeys.add(e.getKeyCode());
+        }
+        @Override
+        public void keyReleased(KeyEvent e){
+            Game.pressedKeys.remove(e.getKeyCode());
+        }
+    }
+    public static class Player2KeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+            Game.pressedKeys.add(e.getKeyCode());
+        }
+        @Override
+        public void keyReleased(KeyEvent e){
+            Game.pressedKeys.remove(e.getKeyCode());
         }
     }
 }
